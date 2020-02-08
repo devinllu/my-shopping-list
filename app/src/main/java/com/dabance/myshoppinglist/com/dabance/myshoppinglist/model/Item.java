68183106243b15eb1d031b8a0a3692cd9f92780e
devinlu <ludevinl@sfu.ca>
2020-02-08 14:16:03 -0800

    item and item manager

--- app/src/main/java/com/dabance/myshoppinglist/com/dabance/myshoppinglist/model/Item.java
+++ app/src/main/java/com/dabance/myshoppinglist/com/dabance/myshoppinglist/model/Item.java
@@ -1,4 +1,23 @@
 package com.dabance.myshoppinglist.com.dabance.myshoppinglist.model;
 
 public class Item {
+    private String name;
+    private int quantity;
+
+    public Item(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void incQuantity() {
+        quantity++;
+    }
 }
--- app/src/main/java/com/dabance/myshoppinglist/com/dabance/myshoppinglist/model/ItemManager.java
+++ app/src/main/java/com/dabance/myshoppinglist/com/dabance/myshoppinglist/model/ItemManager.java
@@ -1,4 +1,32 @@
 package com.dabance.myshoppinglist.com.dabance.myshoppinglist.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemManager {
+    private List<Item> lst = new ArrayList<>();
+
+    public ItemManager(List<Item> lst) {
+        lst.add(new Item("eggs", 1));
+        lst.add(new Item("bread", 1));
+        lst.add(new Item("bananas", 1));
+        lst.add(new Item("apples", 1));
+        lst.add(new Item("muffins", 1));
+        lst.add(new Item("milk", 1));
+        lst.add(new Item("cereal", 1));
+        lst.add(new Item("cheese", 1));
+        lst.add(new Item("chicken breast", 1));
+    }
+
+    public int getLength() {
+        return lst.size();
+    }
+
+    public void addItem(Item item) {
+        lst.add(item);
+    }
+
+    public Item getItem(int index) {
+        return lst.get(index);
+    }
 }
